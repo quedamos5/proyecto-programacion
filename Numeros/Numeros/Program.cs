87ae101a1b765754a9de138dd1a9df0807e6d264
yosi90 <yosi121990@hotmail.es>
2022-12-19 15:40:03 +0000

    Arreglado mal funcionamiento del menú Creada función ejercicio4

--- Numeros/Numeros/Program.cs
+++ Numeros/Numeros/Program.cs
@@ -5,10 +5,14 @@
         int actual;
 
         do
-        {   //Creamos el menú \n nueva línea, \t tabulación
-            Console.Write("\n\n\tElija una opción:\n\n\t1.- ¿Número primo?\n\t2.- Factorial\n\t3.- Convertir número de segundos a H : M : S\n\t4.- 15 primeros números de la " +
-                "serie de Pell\n\t5.- ¿Número de Armstrong?\n\t6.- ¿El número introducido está conformado por cifras totalmente distintas?\n\n\t0.- Salir");
-            actual = Console.ReadKey().KeyChar - 48; //Console readkey para que no haga falta pulsar enter. Cualquier input distinto a las opciones dada saldrá del programa.
+        {
+            Console.Clear(); //Limpiamos la pantalla a cada vuelta al menú
+            //Creamos el menú \n nueva línea, \t tabulación
+            Console.Write("\n\n\t1.- ¿Número primo?\n\t2.- Factorial\n\t3.- Convertir número de segundos a H : M : S\n\t4.- 15 primeros números de la " +
+                "serie de Pell\n\t5.- ¿Número de Armstrong?\n\t6.- ¿El número introducido está conformado por cifras totalmente distintas?\n\n\t0.- Salir\n\n\tElija una opción: ");
+            //Console readkey para que no haga falta pulsar enter
+            actual = Console.ReadKey().KeyChar - 48;
+            Console.Clear();
             switch (actual) //Según la opción seleccionada actuamos
             {
                 case 1:
@@ -30,9 +34,10 @@
                     Parte6(); //Invocación de la función
                     break;
                 default:
+                    //Cualquier input distinto a las opciones dadas saldrá del programa sin acciones
                     break;
             }
-        } while (actual < 1 || actual > 6);
+        } while (actual >= 1 || actual <= 6);
         Console.ReadKey();
     }
 
@@ -53,7 +58,16 @@
 
     private static void Parte4()
     {
-        //Aquí código
+        //La fórmula de la sucesión de pell dados el 0 y el 1 como números iniciales, es:
+        // 2 * numero previo + numero previo al previo
+        // osea el tercer número se calcularía: 2 * 1 + 0 = 2.
+        // el cuarto: 2 * 2 + 1 = 5
+
+        int[] sucesionPell = { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        for (int i = 2; i < 15; i++)
+            sucesionPell[i] = 2 * sucesionPell[i - 1] + sucesionPell[i - 2];
+        Console.Write($"\n\n\tLos primeros 15 números de la sucesión de Pell son:\n\t{string.Join(", ", sucesionPell)}\n\n\tPulsa una tecla para continuar");
+        Console.ReadKey();
     }
 
     private static void Parte5()
