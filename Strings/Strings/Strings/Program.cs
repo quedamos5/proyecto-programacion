6f45de658bd7ce6c10ff432dd2018182b38708c9
Yosi <yosi121990@hotmail.es>
2023-01-16 12:56:46 +0000

    arreglado [Antonio]

--- Strings/Strings/Strings/Program.cs
+++ Strings/Strings/Strings/Program.cs
@@ -11,7 +11,8 @@ internal class Program
             Console.Clear(); //Limpiamos la pantalla a cada vuelta al menú
             //Creamos el menú \n nueva línea, \t tabulación
             Console.Write("\n\n\t1.- ¿Es palíndromo?\n\t2.- Nombre en mayusculas y minusculas\n\t3.- Número de ocurrencias de una letra\n\t4.- Repetir un string" +
-                "las veces que decida el usuario\n\t5.- Repetir cada ocurrencia de una cadena\n\t6.- Dado 2 strings borrar del primero las apariencias del segundo\n\n\t0.- Salir\n\n\tElija una opción: ");
+                "las veces que decida el usuario\n\t5.- Repetir cada ocurrencia de una cadena\n\t6.- Dado 2 strings borrar del primero las apariencias del segundo\n\n\t" +
+                "0.- Salir\n\n\tElija una opción: ");
             //Console readkey para que no haga falta pulsar enter
             actual = Console.ReadKey().KeyChar - 48;
             Console.Clear();
@@ -55,13 +56,38 @@ internal class Program
         else
             Console.WriteLine("No es palíndromo");
         Console.ReadKey();
+
+        static string Reverse(string cadena)
+        {
+            string rev = "";
+            for (int i = cadena.Length - 1; i >= 0; --i)
+                rev += cadena[i];
+            return rev;
+        }
     }
 
-    static string Reverse(string cadena)
+    static void Parte2()
     {
-        string rev = "";
-        for (int i = cadena.Length - 1; i >= 0; --i)
-            rev += cadena[i];
-        return rev;
+        //Código aquí
+    }
+
+    static void Parte3()
+    {
+        //Código aquí
+    }
+
+    static void Parte4()
+    {
+        //Código aquí
+    }
+
+    static void Parte5()
+    {
+        //Código aquí
+    }
+
+    static void Parte6()
+    {
+        //Código aquí
     }
 }
